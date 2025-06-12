8b2de6ddeda1904705b37be1fbc6f1c8bf9ccd33
aarnawa05 <aarnawakoirala@gmail.com>
2025-06-12 17:45:47 -0500

    initialized backend/spring setup

---
+++ backend/sf-catalog/src/main/java/com/nawa/sf_catalog/SfCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.nawa.sf_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/sf-catalog/src/test/java/com/nawa/sf_catalog/SfCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nawa.sf_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
